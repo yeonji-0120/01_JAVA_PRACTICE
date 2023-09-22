99d97be5ef199aaba74147705357ceb20fe1d5e4
yeonji-0120 <noyeonji43@gmail.com>
2023-09-22 18:02:04 +0900

    first commit

---
+++ 01_JAVA_PRACTICE/chap01-literal-and-variable-practice-question/src/main/java/com/ohgiraffers/level02/normal/Application2.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.level02.normal;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        /* 국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤
+         * 총점과 평균을 정수 형태로 출력하세요
+         *
+         * -- 출력 예시 --
+         * 총점 : 201
+         * 평균 : 67
+         *  */
+    }
+}
