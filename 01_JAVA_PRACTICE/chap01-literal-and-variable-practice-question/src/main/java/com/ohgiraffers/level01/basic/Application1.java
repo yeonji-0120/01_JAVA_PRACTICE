c940cc1aedc14090d2620b6ccf2dbd9891c97622
yeonji-0120 <noyeonji43@gmail.com>
2023-09-22 18:24:00 +0900

    test

--- 01_JAVA_PRACTICE/chap01-literal-and-variable-practice-question/src/main/java/com/ohgiraffers/level01/basic/Application1.java
+++ 01_JAVA_PRACTICE/chap01-literal-and-variable-practice-question/src/main/java/com/ohgiraffers/level01/basic/Application1.java
@@ -16,6 +16,7 @@ public class Application1 {
         System.out.println("나누기한 몫 : " + (inum / snum));
         System.out.println("나누기한 나머지 : " + (inum % snum));
 
+        //2023.09.22
         /* 정수형 변수 2개를 선언하여  각 20과 30으로 초기화 한 후
          * 두 수의 더하기, 빼기, 곱하기, 나누기, 나머지를 다음과 같이 출력하세요
          *
