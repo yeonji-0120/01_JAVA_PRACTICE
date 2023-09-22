7eabc129da3eea4e557586c41c2bf85af684458f
yeonji-0120 <noyeonji43@gmail.com>
2023-09-22 18:22:06 +0900

    first

--- 01_JAVA_PRACTICE/chap01-literal-and-variable-practice-question/src/main/java/com/ohgiraffers/level01/basic/Application1.java
+++ 01_JAVA_PRACTICE/chap01-literal-and-variable-practice-question/src/main/java/com/ohgiraffers/level01/basic/Application1.java
@@ -4,6 +4,18 @@ public class Application1 {
 
     public static void main(String[] args) {
 
+        int inum;
+        short snum;
+
+        inum = 20;
+        snum = 30;
+
+        System.out.println("더하기 결과 : " + (inum + snum));
+        System.out.println("뺴기 결과 " + (inum - snum));
+        System.out.println("곱하기 결과 : " + (inum * snum));
+        System.out.println("나누기한 몫 : " + (inum / snum));
+        System.out.println("나누기한 나머지 : " + (inum % snum));
+
         /* 정수형 변수 2개를 선언하여  각 20과 30으로 초기화 한 후
          * 두 수의 더하기, 빼기, 곱하기, 나누기, 나머지를 다음과 같이 출력하세요
          *
